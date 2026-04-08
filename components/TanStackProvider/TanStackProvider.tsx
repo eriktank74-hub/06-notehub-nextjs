b46d2f8a276e71cf1e55d6f2b41d42e1a34ca746
Erik <erik@Eriks-MacBook-Pro.local>
2026-04-08 19:34:31 +0300

    homework

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useMemo } from "react";
 
 
 
@@ -9,7 +10,8 @@ const TanStackProvider = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-    const queryClient = new QueryClient();
+    const queryClient = useMemo(() => new QueryClient(),[]);
+    
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
